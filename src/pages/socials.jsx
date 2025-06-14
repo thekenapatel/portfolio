2efd563163c64d987a322d99faf07cfaa8996650
thekenapatel <kenapatel22gmail.com>
2025-06-14 19:03:55 +0530

    Add new resume PDF and update download link in socials page

--- src/pages/socials.jsx
+++ src/pages/socials.jsx
@@ -64,7 +64,7 @@ const Socials = () => (
                 </div>
             </div>
             <div className='resume'>
-                <a href='/public/resume/Kena Patel Resume.pdf' download>
+                <a href='/public/resume/kenapatelresume.pdf' download>
                     <FileDownloadIcon />
                     <p>Download Resume</p>
                 </a>
